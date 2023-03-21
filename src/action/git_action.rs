3fbbfd98204233ef644d1202daeed7310c9f2890
GangCheng <gang.cheng@lanmudata.com>
2023-03-21 10:31:04 +0800

    format code ,add colour dependency

--- src/action/git_action.rs
+++ src/action/git_action.rs
@@ -1,8 +1,11 @@
+use std::process::abort;
+
 #[allow(dead_code)]
 #[allow(unused)]
 use cmd_lib::{run_cmd, run_fun};
+use colour::{e_blue_ln, e_red_ln};
 use regex::Regex;
-use version_compare::{compare_to, Cmp};
+use version_compare::{Cmp, compare_to};
 
 pub const MIN_GIT_VERSION: &str = "2.23.0";
 
@@ -13,10 +16,13 @@ impl GitAction {
         let git_version_result = run_fun!(git --version);
         let result = match git_version_result {
             Ok(result) => String::from(result),
-            Err(e) => panic!("Get git version error : {}", e),
+            Err(e) => {
+                e_red_ln!("Get git version error : {}", e);
+                abort();
+            }
         };
         if !result.is_empty() {
-            println!("Current git version : {}", result);
+            e_blue_ln!("Current git version : {}", result);
             let version_number_regex = Regex::new(r"\d*\.\d*\.\d*").unwrap();
             let current_version = version_number_regex
                 .find(result.as_str())
@@ -33,80 +39,107 @@ impl GitAction {
     }
 
     pub fn switch_branch(branch_name: String) {
-        println!("[GIT]Switch to branch : {}", branch_name);
+        e_blue_ln!("[GIT]Switch to branch : {}", branch_name);
         run_cmd!("git switch" + branch_name)
-            .map_err(|err| println!("{:?}", err))
+            .map_err(|err| {
+                e_red_ln!("{:?}", err);
+                abort();
+            })
             .ok();
     }
 
     pub fn checkout_branch(branch_name: String) {
-        println!("[GIT]Checkout branch : {}", branch_name);
+        e_blue_ln!("[GIT]Checkout branch : {}", branch_name);
         let name = branch_name;
         run_cmd!(git checkout $name)
-            .map_err(|err| println!("{:?}", err))
+            .map_err(|err| {
+                e_red_ln!("{:?}", err);
+                abort();
+            })
             .ok();
     }
 
     pub fn are_there_anything_changed() -> bool {
-        println!("[GIT]Check whether there are anything changed in current branch");
+        e_blue_ln!("[GIT]Check whether there are anything changed in current branch");
         let git_status_result = run_fun!(git status --porcelain --untracked-files=no);
         let result = match git_status_result {
             Ok(result) => String::from(result),
-            Err(e) => panic!("Get git version error : {}", e),
+            Err(e) => {
+                e_red_ln!("Get git version error : {}", e);
+                abort();
+            }
         };
         !result.is_empty()
     }
 
     pub fn create_new_branch_from_current(branch_name: String) {
-        println!("[GIT]Create a new branch : {}", branch_name);
+        e_blue_ln!("[GIT]Create a new branch : {}", branch_name);
         let name = branch_name;
         run_cmd!(git branch $name)
-            .map_err(|err| println!("{:?}", err))
+            .map_err(|err| {
+                e_red_ln!("{:?}", err);
+                abort();
+            })
             .ok();
     }
 
     pub fn merge_to_current(source_branch_name: String) {
-        println!(
+        e_blue_ln!(
             "[GIT]Merge branch to current,Branch name : {}",
             source_branch_name
         );
         let name = source_branch_name;
         run_cmd!(git merge $name)
-            .map_err(|err| println!("{:?}", err))
+            .map_err(|err| {
+                e_red_ln!("{:?}", err);
+                abort();
+            })
             .ok();
     }
 
     pub fn fetch() {
-        println!("[GIT]Fetch from remote");
+        e_blue_ln!("[GIT]Fetch from remote");
         run_cmd!(git fetch)
-            .map_err(|err| println!("{:?}", err))
+            .map_err(|err| {
+                e_red_ln!("{:?}", err);
+                abort();
+            })
             .ok();
     }
 
     pub fn pull() {
-        println!("[GIT]Pull from remote");
+        e_blue_ln!("[GIT]Pull from remote");
         run_cmd!(git pull)
-            .map_err(|err| println!("{:?}", err))
+            .map_err(|err| {
+                e_red_ln!("{:?}", err);
+                abort();
+            })
             .ok();
     }
 
     pub fn push_branch() {
-        println!("[GIT]Push to remote");
+        e_blue_ln!("[GIT]Push to remote");
         run_cmd!(git push)
-            .map_err(|err| println!("{:?}", err))
+            .map_err(|err| {
+                e_red_ln!("{:?}", err);
+                abort();
+            })
             .ok();
     }
 
     pub fn push_tags() {
-        println!("[GIT]Push tags to remote");
+        e_blue_ln!("[GIT]Push tags to remote");
         run_cmd!(git push --tags)
-            .map_err(|err| println!("{:?}", err))
+            .map_err(|err| {
+                e_red_ln!("{:?}", err);
+                abort();
+            })
             .ok();
     }
 
     pub fn is_branch_exists(branch_name: String, is_remote: bool) -> bool {
         let is_remote_display = if is_remote { "remote" } else { "local" };
-        println!(
+        e_blue_ln!(
             "[GIT]Check if {} branch exists : {}",
             is_remote_display, branch_name
         );
@@ -118,7 +151,10 @@ impl GitAction {
         let verify_result = run_fun!(git rev-parse --verify $branch_to_verify);
         let result = match verify_result {
             Ok(result) => String::from(result),
-            Err(e) => panic!("Check branch exists error : {}", e),
+            Err(e) => {
+                e_red_ln!("Check branch exists error : {}", e);
+                abort();
+            }
         };
         !result.starts_with("fatal:")
     }
@@ -127,31 +163,43 @@ impl GitAction {
         print!("[GIT]Push new branch to remote : {}", branch_name);
         let name = branch_name;
         run_cmd!(git push --set-upstream origin $name)
-            .map_err(|err| println!("{:?}", err))
+            .map_err(|err| {
+                e_red_ln!("{:?}", err);
+                abort();
+            })
             .ok();
     }
 
     pub fn commit(message: String) {
-        println!("[GIT]Commit with message");
+        e_blue_ln!("[GIT]Commit with message");
         run_cmd!(git add ./)
-            .map_err(|err| println!("{:?}", err))
+            .map_err(|err| {
+                e_red_ln!("{:?}", err);
+                abort();
+            })
             .ok();
         let message_data = message;
         run_cmd!(git commit -m $message_data)
-            .map_err(|err| println!("{:?}", err))
+            .map_err(|err| {
+                e_red_ln!("{:?}", err);
+                abort();
+            })
             .ok();
     }
 
     pub fn tag(tag_name: String) {
-        println!("[GIT]Create new tag : {}", tag_name);
+        e_blue_ln!("[GIT]Create new tag : {}", tag_name);
         let tag = tag_name;
         run_cmd!(git tag $tag)
-            .map_err(|err| println!("{:?}", err))
+            .map_err(|err| {
+                e_red_ln!("{:?}", err);
+                abort();
+            })
             .ok();
     }
 
     pub fn get_last_tag_name() -> Option<String> {
-        println!("[GIT]Get latest tag name");
+        e_blue_ln!("[GIT]Get latest tag name");
         let git_tag_result = run_fun!(git rev-list --tags --max-count=1);
         let git_tag = match git_tag_result {
             Ok(output) => Some(String::from(output)),
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+use std::process::abort;
+
+use colour::e_red_ln;
+
 use crate::action::git_action::{GitAction, MIN_GIT_VERSION};
 use crate::support::prompt::get_flow_type_from_prompt;
 use crate::support::prompt::get_package_type_from_prompt;
@@ -9,30 +13,36 @@ fn main() {
     let result = get_package_type_from_prompt();
     let project_type = match result {
         Ok(choice) => choice,
-        Err(_) => panic!("未识别的项目类型，请重试"),
+        Err(_) => {
+            e_red_ln!("未识别的项目类型，请重试");
+            abort();
+        }
     };
     let result = get_flow_type_from_prompt();
     let flow_type = match result {
         Ok(choice) => choice,
-        Err(_) => panic!("未识别的流程类型，请重试"),
+        Err(_) => {
+            e_red_ln!("未识别的流程类型，请重试");
+            abort();
+        }
     };
     println!("Project Type :{}", project_type.to_string());
     println!("Flow Type :{}", flow_type.to_string());
 
     let current_git_version = GitAction::get_git_version();
     if current_git_version.is_none() {
-        println!("未在本机器识别到git命令");
+        e_red_ln!("未在本机器识别到git命令");
         return;
     }
     let is_valid_git_version =
         GitAction::check_git_version(String::from(current_git_version.unwrap()));
     if !is_valid_git_version {
-        println!("本机git命令，版本过低，最小版本 :{}", MIN_GIT_VERSION);
+        e_red_ln!("本机git命令，版本过低，最小版本 :{}", MIN_GIT_VERSION);
         return;
     }
     let last_tag_name = GitAction::get_last_tag_name();
     match last_tag_name {
         Some(value) => println!("最新的Tag名称 : {}", value),
-        None => print!("未获取到最新的Tag名称"),
+        None => e_red_ln!("未获取到最新的Tag名称"),
     };
 }
